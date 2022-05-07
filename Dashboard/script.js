77855b79d694b0954feedb2924506993c2af6f10
Vitor Borges Vieira <101013550+vitorborqge@users.noreply.github.com>
2022-05-07 13:32:02 -0300

    Add files via upload

---
+++ Dashboard/script.js
@@ -0,0 +1,20 @@
+$(document).ready(function() { // é uma pequena caixa pop-up que aparece quando o usuário move o ponteiro do mouse sobre um elemento
+    $(`[data-toggle="tooltip"]`).tooltip();
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
